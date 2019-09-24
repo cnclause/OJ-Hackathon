2f5e11a2bd9fa211e694ed4caa2ebffc321a00bf
github username <github email address>
2019-09-24 13:09:30 -0600

    added seed characters and added index to all controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources "characters", only: [:index]
+  resources "exhibits", only: [:index]
+  resources "trials", only: [:index]
 end
